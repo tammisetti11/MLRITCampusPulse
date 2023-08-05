c1a756074317f1c390842b5813715c0fc9f660e6
tammisetti11 <tammisettinaveen@gmail.com>
2023-08-05 10:00:00 -0500

    deployment changes

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: axios.get("https://campuspulse-api.onrender.com/api/events")
 });
 
 // Automatically attach JWT to requests
